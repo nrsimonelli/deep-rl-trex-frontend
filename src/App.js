beac2e5df3ad3883dd7251aed4436e4ecef10b2a
Nicholas <nrsimonelli@gmail.com>
2020-10-08 18:49:26 -0500

    started flexbox and app divs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          World Records Coming Soon...
-        </p>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+
+  render() {
+    return (
+      <div className='App'>
+        <div className='title'>
+          <div>deep rl chrome</div>
+          <div></div>
+          <div>something</div>
+          <div>/</div>
+          <div>icons</div>
+        </div>
+        <div className='row-container'>
+          <div className='recent-runs'>
+            recent runs here
+          </div>
+          <div className='model-list'>
+            <div className='list'>
+            list
+            </div>
+            <div className='graph'>
+            graph / chart here
+            </div>
+          </div>
+          
+          
+        </div>
+        <div className='footer'>
+            footer here
+        </div>
+        
+      </div>
+    ); // end return
+  } // end render
+} // end class
 
 export default App;
